f29bc2f9ee583266c7b6cb1f12d4d9942c956350
jaimehrubiks <jaimehidalgo2@hotmail.com>
2017-03-19 19:42:55 +0100

    removed example code

--- app/routes/netflow.js
+++ app/routes/netflow.js
@@ -50,42 +50,42 @@ function test(flow){
 }
 
 
-setTimeout(function(){
-   var data = { header:
-   { version: 9,
-     count: 2,
-     uptime: 104748,
-     seconds: 1014940904,
-     sequence: 1,
-     sourceId: 0 },
-  flows:
-   [ { last_switched: 77924,
-       first_switched: 59328,
-       in_bytes: 1,
-       in_pkts: 35,
-       input_snmp: 3,
-       output_snmp: 0,
-       ipv4_src_addr: '192.168.1.5',
-       ipv4_dst_addr: '192.168.0.1',
-       protocol: 1,
-       src_tos: 0,
-       l4_src_port: 0,
-       l4_dst_port: 2048,
-       flow_sampler_id: 0,
-       unknown_type_51: '00',
-       ipv4_next_hop: '0.0.0.0',
-       dst_mask: 24,
-       src_mask: 24,
-       tcp_flags: 16,
-       direction: 0,
-       dst_as: 0,
-       src_as: 0,
-       fsId: 256 } ],
-  templates: { '192.168.0.1:60681': { '256': [Object] } },
-  rinfo: { address: '192.168.0.1', family: 'IPv4', port: 60681, size: 164 },
-  }
-  test(data)
-},4000)
+// setTimeout(function(){
+//    var data = { header:
+//    { version: 9,
+//      count: 2,
+//      uptime: 104748,
+//      seconds: 1014940904,
+//      sequence: 1,
+//      sourceId: 0 },
+//   flows:
+//    [ { last_switched: 77924,
+//        first_switched: 59328,
+//        in_bytes: 1,
+//        in_pkts: 35,
+//        input_snmp: 3,
+//        output_snmp: 0,
+//        ipv4_src_addr: '192.168.1.5',
+//        ipv4_dst_addr: '192.168.0.1',
+//        protocol: 1,
+//        src_tos: 0,
+//        l4_src_port: 0,
+//        l4_dst_port: 2048,
+//        flow_sampler_id: 0,
+//        unknown_type_51: '00',
+//        ipv4_next_hop: '0.0.0.0',
+//        dst_mask: 24,
+//        src_mask: 24,
+//        tcp_flags: 16,
+//        direction: 0,
+//        dst_as: 0,
+//        src_as: 0,
+//        fsId: 256 } ],
+//   templates: { '192.168.0.1:60681': { '256': [Object] } },
+//   rinfo: { address: '192.168.0.1', family: 'IPv4', port: 60681, size: 164 },
+//   }
+//   test(data)
+// },4000)
 
 module.exports = {
     listen: function(port){ collector.listen(port) }
